a6180a03a6ea8bf8392bd8e4d552a410967471c3
Christian <mrchristianm6@gmail.com>
2025-12-13 21:40:46 +1100

    Changed style of homepage to monochrome and added tab bar on homescreen

--- backend/src/main/java/com/gymbuddy/controller/HomeController.java
+++ backend/src/main/java/com/gymbuddy/controller/HomeController.java
@@ -1,14 +1,22 @@
 package com.gymbuddy.controller;
 
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 @RestController
 public class HomeController {
 
-    @GetMapping("/home")
-    public String home() {
-        return "GymBuddy";
+    @GetMapping(value = "/home", produces = MediaType.TEXT_HTML_VALUE)
+    public String home() throws IOException {
+        Resource resource = new ClassPathResource("static/home.html");
+        return StreamUtils.copyToString(resource.getInputStream(), StandardCharsets.UTF_8);
     }
 }
 
